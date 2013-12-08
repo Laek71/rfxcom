7fdad225cdc004adc0cd6f5714b4afd751f4f646
Laek71 <larsake.ekstrand@gmail.com>
2013-12-08 18:59:31 +0100

    Added debug argument

--- rfxcom.js
+++ rfxcom.js
@@ -7,6 +7,11 @@ var rfxcom = require('rfxcom');
 var rfxtrx = new rfxcom.RfxCom("/dev/ttyUSB0", {debug: false});
 
 /*
+ * Check for debug argument
+ */
+var arg2 = process.argv[2];
+
+/*
  * Constants
  */
 var MODBUS_PORT = 1502;
@@ -20,6 +25,7 @@ var i=0;
 var handlers = {};
 var nbr_of_sensors=0;
 var topic;
+var debugflag=0;
 
 /*
 ** Event: On Oregon Scientific temp and humidity sensor
@@ -28,7 +34,12 @@ rfxtrx.on("th1", function (evt) {
    // Oregon Scientific sensors 
    //console.log("Event %s, %s", evt.subtype, evt.id); 
    //console.log("Temp: %s, Hum:%s", evt.temperature, evt.humidity);
-
+   
+   // Check arguments if debug
+   if(arg2 == 'debug') {
+	debugflag = 1;
+   }
+  
    /*
     * Check which sensor and fill input registers with data
     */
@@ -36,25 +47,25 @@ rfxtrx.on("th1", function (evt) {
    case "0xFB01": 
    		input_register[0] = evt.temperature * 10;
    		input_register[1] = evt.humidity * 10;
-   		//console.log("Ute: temp = %s, hum = %s", input_register[0], input_register[1]);
+   		if(debugflag==1) {console.log("Ute: temp = %s, hum = %s", input_register[0], input_register[1]);}
    break;
    case "0x6F02": 
    		input_register[2] = evt.temperature * 10;
 		input_register[3] = evt.humidity * 10;
-		//console.log("Kaellare: temp = %s, hum = %s", input_register[2], input_register[3]);
+		if(debugflag==1) {console.log("Kaellare: temp = %s, hum = %s", input_register[2], input_register[3]);}
    break;
    case "0x7004": 
    		input_register[4] = evt.temperature * 10;
 		input_register[5] = evt.humidity * 10;
-		//console.log("Vind: temp = %s, hum = %s", input_register[4], input_register[5]);
+		if(debugflag==1) {console.log("Vind: temp = %s, hum = %s", input_register[4], input_register[5]);}
    break;
    case "0x1A04": 
    		input_register[6] = evt.temperature * 10;
 		input_register[7] = evt.humidity * 10;
-		//console.log("Inne: temp = %s, hum = %s", input_register[6], input_register[7]);
+		if(debugflag==1) {console.log("Inne: temp = %s, hum = %s", input_register[6], input_register[7]);}
    break;
    default:
-	   //console.log("Unknown sensor (%s) = %s", evt.id, evt.temperature);
+	   if(debugflag==1) {console.log("Unknown sensor (%s) = %s", evt.id, evt.temperature);}
    }
    
 });
